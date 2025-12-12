5f04c36f5b6fc4a13ee8db00fcbabfd047a21c4d
Jessy <contact@moysus.com>
2025-12-12 09:58:21 +0100

    introduce reusable form elements

---
+++ src/website/forms/FormElements.tsx
@@ -0,0 +1,62 @@
+import { Step } from "@/models/Step";
+import clsx from "clsx";
+import { ReactNode } from "react";
+import { FormState } from "react-hook-form";
+import { Button } from "../comps/Button";
+import { Icon } from "../comps/Icon";
+import { Spinner } from "../comps/Spinner";
+import { StepTranslation } from "../translation/StepTranslation";
+
+export namespace FormElements {
+  type TitleProps = {
+    stepType: Step.Type;
+  };
+  export function Title({ stepType }: TitleProps) {
+    return <h1 className="text-2xl font-extralight text-c-dim">{StepTranslation.type(stepType)}</h1>;
+  }
+
+  type ButtonBarProps = {
+    className?: string;
+    formState: FormState<{}>;
+    existing?: Step;
+    onSubmit: () => unknown;
+    onDelete: (id: string) => unknown;
+    onCancel: () => unknown;
+  };
+  export function ButtonBar({ className, formState, existing, onSubmit, onDelete, onCancel }: ButtonBarProps) {
+    return (
+      <div className={clsx("flex flex-row-reverse justify-between gap-4", className)}>
+        <div className="flex gap-4">
+          {!formState.isSubmitting && <Button onClick={onCancel}>Cancel</Button>}
+          <Button disabled={formState.isSubmitting} onClick={onSubmit} className="border-c-success text-c-success hover:bg-c-success/20">
+            {formState.isSubmitting ? <Spinner className="border-c-success" /> : existing ? "Update Step" : "Add Step"}
+          </Button>
+        </div>
+        {existing && !formState.isSubmitting && (
+          <Button className="border-c-error! text-c-error hover:bg-c-error/20" onClick={() => onDelete(existing.id)}>
+            Delete Step
+          </Button>
+        )}
+      </div>
+    );
+  }
+
+  type DescriptionOrErrorProps = {
+    formState: FormState<{}>;
+    clearErrors: () => unknown;
+    children?: ReactNode;
+  };
+  export function DescriptionOrError({ formState, clearErrors, children }: DescriptionOrErrorProps) {
+    if (formState.errors.root?.message) {
+      return (
+        <div className="border-3 border-c-error p-3 rounded-lg flex justify-between items-start">
+          <pre className="text-c-error">{formState.errors.root.message}</pre>
+          <button className="cursor-pointer" onClick={() => clearErrors()}>
+            <Icon variant="close" className="size-5" />
+          </button>
+        </div>
+      );
+    }
+    return children || null;
+  }
+}
--- src/website/forms/step/CompressionForm.tsx
+++ src/website/forms/step/CompressionForm.tsx
@@ -1,10 +1,7 @@
 import { Step } from "@/models/Step";
 import clsx from "clsx";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { Button } from "../../comps/Button";
-import { Icon } from "../../comps/Icon";
-import { Spinner } from "../../comps/Spinner";
-import { StepTranslation } from "../../translation/StepTranslation";
+import { FormElements } from "../FormElements";
 import { FormHelper } from "../FormHelper";
 
 type Form = {
@@ -51,7 +48,7 @@ export function CompressionForm({ id, existing, onSave, onDelete, onCancel, clas
   return (
     <div className={clsx(className, "u-subgrid font-light")}>
       <div className="col-span-6 pr-3">
-        <h1 className="text-2xl font-extralight text-c-dim">{StepTranslation.type(Step.Type.compression)}</h1>
+        <FormElements.Title stepType={Step.Type.compression} />
 
         <fieldset disabled={formState.isSubmitting} className="mt-8 flex flex-col gap-4">
           <div className="flex items-center">
@@ -72,35 +69,19 @@ export function CompressionForm({ id, existing, onSave, onDelete, onCancel, clas
           </div>
         </fieldset>
 
-        <div className="mt-12 flex flex-row-reverse justify-between gap-4">
-          <div className="flex gap-4">
-            {!formState.isSubmitting && <Button onClick={onCancel}>Cancel</Button>}
-            <Button
-              disabled={formState.isSubmitting}
-              onClick={handleSubmit(submit)}
-              className="border-c-success text-c-success hover:bg-c-success/20"
-            >
-              {formState.isSubmitting ? <Spinner className="border-c-success" /> : existing ? "Update Step" : "Add Step"}
-            </Button>
-          </div>
-          {existing && !formState.isSubmitting && (
-            <Button className="border-c-error! text-c-error hover:bg-c-error/20" onClick={() => onDelete(existing.id)}>
-              Delete Step
-            </Button>
-          )}
-        </div>
+        <FormElements.ButtonBar
+          className="mt-12"
+          formState={formState}
+          existing={existing}
+          onSubmit={handleSubmit(submit)}
+          onDelete={onDelete}
+          onCancel={onCancel}
+        />
       </div>
       <div className="col-span-6 pl-3 border-l-2 border-c-dim/20">
-        {formState.errors.root?.message ? (
-          <div className="border-3 border-c-error p-3 rounded-lg flex justify-between items-start">
-            <pre className="text-c-error">{formState.errors.root.message}</pre>
-            <button className="cursor-pointer" onClick={() => clearErrors()}>
-              <Icon variant="close" className="size-5" />
-            </button>
-          </div>
-        ) : (
+        <FormElements.DescriptionOrError formState={formState} clearErrors={clearErrors}>
           <p>This step can be used for compressing artifacts</p>
-        )}
+        </FormElements.DescriptionOrError>
       </div>
     </div>
   );
