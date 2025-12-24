63fac26cdd3503687b3186cbf46a7d923d90f519
Jessy <contact@moysus.com>
2025-12-24 16:10:29 +0100

    add buffering support

--- src/website/clients/SocketClient.ts
+++ src/website/clients/SocketClient.ts
@@ -1,18 +1,23 @@
 import { Generate } from "@/helpers/Generate";
 import { ServerMessage } from "@/models/socket/ServerMessage";
 
+type Subscription = {
+  id: string;
+  type: ServerMessage.Type;
+  callback: (message: ServerMessage) => unknown;
+};
+
 export class SocketClient {
-  private readonly callbacks: Record<ServerMessage.Type, Record<string, (message: ServerMessage) => void>> = {
-    [ServerMessage.Type.execution_update]: {},
-  };
+  private readonly subscriptions: Subscription[] = [];
+  private readonly pauseBuffers: Map<string, ServerMessage[]> = new Map();
   private readonly latestMessages: Partial<Record<ServerMessage.Type, ServerMessage>> = {};
 
   public initialize() {
     // TODO: automatically try reconnecting on close !
     new WebSocket("/socket").addEventListener("message", ({ data }) => {
       const message = ServerMessage.parse(JSON.parse(data));
-      Object.values(this.callbacks[message.type]).forEach((callback) => callback(message));
       this.latestMessages[message.type] = message;
+      this.deliverMessage(message);
     });
   }
 
@@ -21,32 +26,63 @@ export class SocketClient {
     callback,
     replayLatestMessage = false,
   }: SocketClient.SubscribeOptions<T>): string => {
-    const id = `CB${Generate.shortRandomString()}`;
-    this.callbacks[type as ServerMessage.Type][id] = (message) => {
-      callback(message as Extract<ServerMessage, { type: T }>);
+    const subscription = {
+      id: `CB${Generate.shortRandomString()}`,
+      type,
+      callback: callback as Subscription["callback"],
     };
+    this.subscriptions.push(subscription);
     if (replayLatestMessage) {
       const latestMessage = this.latestMessages[type];
       if (latestMessage) {
-        callback(latestMessage as Extract<ServerMessage, { type: T }>);
+        this.deliverMessage(latestMessage, subscription);
       }
     }
-    return id;
+    return subscription.id;
   };
 
   public unsubscribe = (id: string): void => {
-    Object.values(this.callbacks).forEach((category) => {
-      if (category[id]) {
-        delete category[id];
+    const index = this.subscriptions.findIndex((sub) => sub.id === id);
+    if (index >= 0) {
+      this.subscriptions.splice(index, 1);
+    }
+    this.pauseBuffers.delete(id);
+  };
+
+  public pauseAndBuffer(id: string): void {
+    this.pauseBuffers.set(id, []);
+  }
+
+  public continueAndDrain(id: string): void {
+    const subscription = this.subscriptions.find((sub) => sub.id === id);
+    const bufferedMessages = this.pauseBuffers.get(id);
+    if (subscription && bufferedMessages) {
+      this.pauseBuffers.delete(id);
+      setTimeout(() => {
+        bufferedMessages.forEach((message) => this.deliverMessage(message, subscription));
+      });
+    }
+  }
+
+  private deliverMessage(message: ServerMessage, specificSubscription?: Subscription) {
+    const deliveryTargets: Subscription[] = specificSubscription
+      ? [specificSubscription]
+      : this.subscriptions.filter(({ type }) => type === message.type);
+    deliveryTargets.forEach(({ id, callback }) => {
+      const buffer = this.pauseBuffers.get(id);
+      if (buffer) {
+        buffer.push(message);
+      } else {
+        callback(message);
       }
     });
-  };
+  }
 }
 
 export namespace SocketClient {
   export type SubscribeOptions<T extends ServerMessage.Type> = {
     type: T;
-    callback(message: Extract<ServerMessage, { type: T }>): void;
+    callback(message: Extract<ServerMessage, { type: T }>): unknown;
     replayLatestMessage?: boolean;
   };
 }
