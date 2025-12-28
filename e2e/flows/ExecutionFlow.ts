6b05a0c15ca573c92611cde54ac6941e45bd364b
Jessy <contact@moysus.com>
2025-12-28 18:13:54 +0100

    commit

--- e2e/flows/ExecutionFlow.ts
+++ e2e/flows/ExecutionFlow.ts
@@ -5,6 +5,8 @@ export namespace ExecutionFlow {
     expectedOutcome: "success" | "error";
   };
   export async function executeCurrentPipeline(page: Page, { expectedOutcome = "success" } = {} as ExecutePipelineOptions) {
+    const name = await page.getByRole("heading", { level: 1 }).textContent();
+    await expect(page).toHaveTitle(`${name} | Pipelines | Brespi`);
     await page.getByRole("button", { name: "Execute", exact: true }).click();
     if (expectedOutcome === "success") {
       await expect(page.getByText("This execution has succeeded")).toBeVisible();
