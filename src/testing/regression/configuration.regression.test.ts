b91b39e8e602af2b26dd9d90653d0343a8768268
Jessy <contact@moysus.com>
2026-02-01 16:22:58 +0100

    commit

--- src/testing/regression/configuration.regression.test.ts
+++ src/testing/regression/configuration.regression.test.ts
@@ -1,10 +1,32 @@
 import { describe, expect, it } from "bun:test";
+import { basename, join } from "path";
+import { TestUtils } from "../TestUtils.test";
 
-describe("REGRESSION > configuration", () => {
-  it("example configuration test 1", () => {
-    expect(1 + 1).toEqual(2);
-  });
-  it("example configuration test 2", () => {
-    expect(1 + 1).toEqual(2);
+describe("REGRESSION > configuration", async () => {
+  type TestCase = {
+    filename: string;
+    json: any;
+  };
+  async function readConfigurationJsons(folder: string): Promise<TestCase[]> {
+    const glob = new Bun.Glob("*.json");
+    const files = await Array.fromAsync(glob.scan({ cwd: folder, absolute: true }));
+    return await Promise.all(
+      [...files].map<Promise<TestCase>>(async (file) => ({
+        filename: basename(file),
+        json: await Bun.file(file).json(),
+      })),
+    );
+  }
+
+  const suitePath = join(import.meta.dir, "configurations");
+  const configurationJsons = await readConfigurationJsons(suitePath);
+  if (configurationJsons.length === 0) {
+    throw new Error(`Couldn't find configuration JSONs: ${suitePath}`);
+  }
+
+  const collection = TestUtils.createCollection("filename", configurationJsons);
+  it.each(collection.testCases)("%s", (testCase) => {
+    const { json } = collection.get(testCase);
+    console.log(json);
   });
 });
--- src/testing/regression/database.regression.test.ts
+++ src/testing/regression/database.regression.test.ts
@@ -1,6 +1,12 @@
 import { describe, expect, it } from "bun:test";
+import { join } from "path";
 
-describe("REGRESSION > database", () => {
+describe("REGRESSION > database", async () => {
+  const databasePath = join(import.meta.dir, "databases", "db.sqlite");
+  if (!(await Bun.file(databasePath).exists())) {
+    throw new Error(`Regression database not found: ${databasePath}`);
+  }
+
   it("example database test 1", () => {
     expect(1 + 1).toEqual(2);
   });
