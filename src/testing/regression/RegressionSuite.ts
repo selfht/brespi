5e585896eed7adee07ea56f1ae6fa96bfdaaa082
Jessy <contact@moysus.com>
2026-02-04 05:27:28 +0100

    commit

---
+++ src/testing/regression/RegressionSuite.ts
@@ -0,0 +1,111 @@
+import * as schema from "@/drizzle/schema";
+import { $action, $execution, $notificationPolicyMetadata, $scheduleMetadata } from "@/drizzle/schema";
+import { initializeSqlite, Sqlite } from "@/drizzle/sqlite";
+import { Env } from "@/Env";
+import { describe, test } from "bun:test";
+import { getTableName, InferInsertModel } from "drizzle-orm";
+import { basename, dirname, join } from "path";
+
+describe("regression suite management", async () => {
+  test.skip("manually update the regression suite", async () => {
+    // This is a playground where the regression suite can be manually updated
+  });
+});
+
+export namespace RegressionSuite {
+  const suitePath = join(import.meta.dir, "suite");
+  /**
+   * Helper functions for reading/writing configuration
+   */
+  export namespace Configuration {
+    export type ConfigurationJson = {
+      filename: string;
+      json: any;
+    };
+    export async function readJsons(): Promise<ConfigurationJson[]> {
+      const glob = new Bun.Glob("*.json");
+      const files = await Array.fromAsync(glob.scan({ cwd: suitePath, absolute: true }));
+      const output = await Promise.all(
+        [...files].map<Promise<ConfigurationJson>>(async (file) => ({
+          filename: basename(file),
+          json: await Bun.file(file).json(),
+        })),
+      );
+      if (output.length === 0) {
+        throw new Error(`Couldn't find configuration JSONs: ${suitePath}`);
+      }
+      return output;
+    }
+    export async function writeJsons(input: ConfigurationJson[]): Promise<void> {
+      await Promise.all(
+        input.map(async ({ filename, json }) => {
+          const path = join(suitePath, filename);
+          await Bun.write(path, JSON.stringify(json, null, 2));
+        }),
+      );
+    }
+  }
+
+  /**
+   * Helper functions for reading/writing configuration
+   */
+  export namespace Database {
+    type Schema = typeof schema;
+    type TableKey = Exclude<Extract<keyof Schema, string>, `${string}Relations`>;
+    type Tables = {
+      [K in TableKey]: Schema[K];
+    };
+    type TableRecords = {
+      [K in keyof Tables]: Array<InferInsertModel<Tables[K]>>;
+    };
+    export function getTableOverview() {
+      const tables: Tables = {
+        $execution: $execution,
+        $action: $action,
+        $scheduleMetadata: $scheduleMetadata,
+        $notificationPolicyMetadata: $notificationPolicyMetadata,
+      };
+      return Object.values(tables).map((table) => ({
+        table,
+        tableName: getTableName(table),
+      }));
+    }
+
+    const databasePath = join(suitePath, "db.sqlite");
+    export async function getRegressionDatabaseCopy(): Promise<Sqlite> {
+      const file = Bun.file(databasePath);
+      if (!(await file.exists())) {
+        throw new Error(`Regression database not found: ${databasePath}`);
+      }
+      const databaseCopyPath = join(dirname(databasePath), "dbcopy.sqlite");
+      await Bun.write(databaseCopyPath, file);
+      return await initializeSqlite({ X_BRESPI_DATABASE: databaseCopyPath } as Env.Private);
+    }
+    export async function updateRegressionDatabase({
+      $execution: executions,
+      $action: actions,
+      $scheduleMetadata: scheduleMetadatas,
+      $notificationPolicyMetadata: notificationPolicyMetadatas,
+    }: TableRecords) {
+      const file = Bun.file(databasePath);
+      if (await file.exists()) {
+        await file.delete();
+      }
+      const sqlite = await initializeSqlite({ X_BRESPI_DATABASE: databasePath } as Env.Private);
+      if (executions.length > 0) {
+        await sqlite.insert($execution).values(executions);
+      }
+      if (actions.length > 0) {
+        await sqlite.insert($action).values(actions);
+      }
+      if (scheduleMetadatas.length > 0) {
+        await sqlite.insert($scheduleMetadata).values(scheduleMetadatas);
+      }
+      if (notificationPolicyMetadatas.length > 0) {
+        await sqlite.insert($notificationPolicyMetadata).values(notificationPolicyMetadatas);
+      }
+      sqlite.close();
+      return databasePath;
+    }
+  }
+}
--- src/testing/regression/regression.test.ts
+++ src/testing/regression/regression.test.ts
@@ -1,11 +1,11 @@
 import { Configuration } from "@/models/Configuration";
 import { describe, expect, it } from "bun:test";
 import { TestUtils } from "../TestUtils.test";
-import { RegressionSuiteManagement } from "./RegressionSuiteManagement";
+import { RegressionSuite } from "./RegressionSuite";
 
 describe("regression", () => {
   describe("configuration parsing", async () => {
-    const collection = TestUtils.createCollection("filename", await RegressionSuiteManagement.readConfigurationJsons());
+    const collection = TestUtils.createCollection("filename", await RegressionSuite.Configuration.readJsons());
     it.each(collection.testCases)("%s", (testCase) => {
       // given
       const { json } = collection.get(testCase);
@@ -16,8 +16,8 @@ describe("regression", () => {
   });
 
   describe("database querying", async () => {
-    const database = await RegressionSuiteManagement.getRegressionDatabaseCopy();
-    const collection = TestUtils.createCollection("tableName", RegressionSuiteManagement.getTables());
+    const database = await RegressionSuite.Database.getRegressionDatabaseCopy();
+    const collection = TestUtils.createCollection("tableName", RegressionSuite.Database.getTableOverview());
     it.each(collection.testCases)("%s", async (testCase) => {
       // given
       const { table } = collection.get(testCase);
